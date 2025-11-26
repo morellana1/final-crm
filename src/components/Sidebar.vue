04515d17cd88f290ae8e2b8271c776933e1b5a74
unknowncode44 <80708560+unknowncode44@users.noreply.github.com>
2025-11-26 17:03:37 -0300

    avance 1

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+
+const isActive = (path: string) => {
+    return route.path.startsWith(path)
+    ? "bg-gray-200 dark:bg-gray-700 font-medium"
+    : "";
+};
+
+interface NavItems {
+    name: string,
+    path: string,
+}
+
+const navItems: NavItems[] = [
+    {name: "Contactos", path: "/contacts"},
+    {name: "Actividades", path: "/activities"},
+]
+
+</script>
+
+<template>
+    <aside class="w-64 bg-white dark:bg-gray-800 border-r boder-gray-200 dark:border-gray-700 p-4">
+        <h1 class="text-xl font-bold text-gray-700 dark:text-gray-200 mb-6">
+            Mini CRM
+        </h1>
+
+        <nav>
+            <ul class="flex flex-col gap-2">
+                <li v-for="(link, index) in navItems" :key="index" >
+                    <RouterLink
+                    :to="link.path"
+                    class="block px-3 py-2 rounded-lg text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700"
+                    :class="isActive(link.path)"
+                    >
+                    {{ link.name }}
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue';
+
+
+</script>
+
+<template>
+    <div class="flex min-h-screen bg-gray-100 dark:bg-gray-900">
+        <Sidebar/>
+        <main class="flex-1 p-6">
+            <RouterView/>
+        </main>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
